d8d4e4fa3e9254a2e31f8f74347c2dc69c53042d
Patrick Marck D <patrickmarckdulaca@gmail.com>
2025-03-09 22:22:09 +0800

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   env: "prod",
-  SERVER_URL_444: "https://api.gcportal.space:444/api/",
-  SERVER_URL: "https://api.gcportal.space/api/",
-  WEBSOCKET: "https://api.gcportal.space:444",
+  SERVER_URL_444: "http://localhost:3002/api/",
+  SERVER_URL: "http://localhost:3001/api/",
+  WEBSOCKET: "http://localhost:3002/",
 };
