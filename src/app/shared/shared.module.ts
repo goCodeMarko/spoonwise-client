bdcb7008f34279aea7df22616ae604e49435a4da
Patrick Marck D <patrickmarckdulaca@gmail.com>
2025-05-21 20:38:14 +0800

    week 8

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -35,7 +35,7 @@ import { OrderListComponent } from "./components/order-list/order-list.component
 import { ChatListComponent } from "./components/chat-list/chat-list.component";
 import { ChatComponent } from "./components/chat/chat.component";
 import { TimeAgoPipe } from "./pipes/time-ago.pipe";
-import { InfiniteScrollModule } from "ngx-infinite-scroll";
+
 @NgModule({
   declarations: [
     AccordionAnchorDirective,
@@ -71,7 +71,6 @@ import { InfiniteScrollModule } from "ngx-infinite-scroll";
     MatSlideToggleModule,
     SwiperModule,
     NgxStarRatingModule,
-    InfiniteScrollModule,
   ],
   exports: [
     AccordionAnchorDirective,
