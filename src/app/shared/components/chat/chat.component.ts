5b0726d4074caa349dd05a665df1dde9da97db4b
Patrick Marck D <patrickmarckdulaca@gmail.com>
2025-08-08 23:34:34 +0800

    Update #10

--- src/app/shared/components/chat/chat.component.ts
+++ src/app/shared/components/chat/chat.component.ts
@@ -31,6 +31,7 @@ import { sendMessageSuccess } from "../../../shared/store/chat/chat.actions";
 import { HttpRequestService } from "src/app/http-request/http-request.service";
 import { SocketService } from "../../socket/socket.service";
 import { take } from "rxjs/operators";
+import imageCompression from "browser-image-compression";
 
 @Component({
   selector: "app-chat",
@@ -223,7 +224,7 @@ export class ChatComponent implements OnInit, OnDestroy {
   removeForUpload() {
     this.forUploadImage = "";
   }
-  onFileChange(event: Event): void {
+  async onFileChange(event: Event): Promise<any> {
     const input = event.target as HTMLInputElement; // Cast the event target to HTMLInputElement to access the files
     if (input.files && input.files[0]) {
       if (_.size(input.files) == 1) {
@@ -231,23 +232,36 @@ export class ChatComponent implements OnInit, OnDestroy {
         const file = input.files[0]; // Get the first selected file
         const reader = new FileReader(); // Create a FileReader to read the file
 
-        reader.readAsDataURL(file); // Read the file as a Base64 data URL
-
-        reader.onload = (e: ProgressEvent<FileReader>) => {
-          // Define what to do when file reading is complete
-          const imgSrc = e.target!.result as string; // Get the Base64 image string
-          const image = new Image();
-
-          this.forUploadImage = imgSrc;
-
-          image.src = imgSrc;
-          image.onload = () => {
-            const width = image.width; // Get image width
-            const height = image.height; // Get image height
-
-            const id = Math.random().toString(36).substring(2, 9);
-          };
+        // Compression settings
+        const options = {
+          maxSizeMB: 0.6, // 0.6 MB = ~600KB
+          maxWidthOrHeight: 1920,
+          useWebWorker: true,
         };
+
+        try {
+          console.log("-------------file", file);
+          const compressedFile = await imageCompression(file, options);
+          console.log("-------------compressedFile", compressedFile);
+          reader.readAsDataURL(compressedFile); // Read the file as a Base64 data URL
+
+          reader.onload = (e: ProgressEvent<FileReader>) => {
+            const imgSrc = e.target!.result as string; // Get the Base64 image string
+            const image = new Image();
+            console.log("-----img0", imgSrc);
+            this.forUploadImage = imgSrc;
+
+            image.src = imgSrc;
+            image.onload = () => {
+              const width = image.width; // Get image width
+              const height = image.height; // Get image height
+
+              const id = Math.random().toString(36).substring(2, 9);
+            };
+          };
+        } catch (error) {
+          console.error("Compression error:", error);
+        }
       }
     }
   }
