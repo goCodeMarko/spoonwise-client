83584d894974b295a73a6ab2eea62148e12f5514
Patrick Marck D <patrickmarckdulaca@gmail.com>
2025-08-09 00:58:37 +0800

    Update #10

--- src/app/shared/components/map/map.component.ts
+++ src/app/shared/components/map/map.component.ts
@@ -154,7 +154,7 @@ export class MapComponent
       fadeAnimation: false,
     }).setView([0, 0], 1);
 
-    L.tileLayer("https://{s}tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution:
         'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
       maxZoom: 18,
