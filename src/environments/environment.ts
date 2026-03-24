9b8a4757f1a79e4ac3dd8ea4f766db0a16c435cb
Patrick Marck D <patrickmarckdulaca@gmail.com>
2026-03-24 13:56:44 +0800

    Update #30

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   env: "uat",
-  SERVER_URL_MAIN: "https://api-2.spoonwise.space/api/",
-  SERVER_URL_CLUSTERS: "https://api-1.spoonwise.space/api/",
-  WEBSOCKET_MAIN: "https://api-2.spoonwise.space/",
+  SERVER_URL_MAIN: "https://api-1.spoonwise.space/api/",
+  SERVER_URL_CLUSTERS: "https://api-2.spoonwise.space/api/",
+  WEBSOCKET_MAIN: "https://api-1.spoonwise.space/",
 };
